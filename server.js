7bf4081f84e957b5bdd3bbbeeece0a7fe2771c76
kaykecastrosi <kaykecastrosi@gmail.com>
2020-07-04 04:14:42 +0000

    removed cors

--- server.js
+++ server.js
@@ -1,11 +1,9 @@
 const express = require("express")
 const mongoose = require('mongoose')
-const cors = require('cors')
-require('dotenv/config')
+require("dotenv/config")
 
 const app = express()
 app.use(express.json())
-app.use(cors)
 
 mongoose.connect(process.env.MONGOOSE_URI, { useNewUrlParser: true })
 require('./src/models/Task')
