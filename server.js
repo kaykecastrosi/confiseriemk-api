d821d68f6313094fb033673d452d7cdd998af43c
kaykecastrosi <kaykecastrosi@gmail.com>
2020-07-04 16:04:10 +0000

    added cors

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require("express")
 const mongoose = require('mongoose')
+const cors = require('cors')
 require("dotenv/config")
 
 const app = express()
+app.use(cors())
 app.use(express.json())
 
 mongoose.connect(process.env.MONGOOSE_URI, { useNewUrlParser: true })
