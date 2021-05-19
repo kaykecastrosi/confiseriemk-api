1a27745442d50b7808c611cde8ff5dd6d8567aab
kaykecastrosi <kaykecastrosi@gmail.com>
2021-05-19 18:28:41 +0000

    desc

--- src/models/Product.js
+++ src/models/Product.js
@@ -5,6 +5,11 @@ const ProductSchema = new mongoose.Schema({
         type: String,
         required: true
     },
+    description: {
+        type: String,
+        required: true
+        
+    },
     price: {
         type: Number,
         required: true
